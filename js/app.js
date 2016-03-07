dde192ba8d6e1e4c70ece186427a217203f88d8e
galdinorosas <dino.rosas@gmail.com>
2016-03-07 01:36:30 -0800

    cleaned up code.

--- js/app.js
+++ js/app.js
@@ -1,51 +1,44 @@
-
 $(document).ready(function() {
 
-	var userInput = prompt("Please enter a number in the box below.","Only whole numbers please");
+    var userInput = prompt("Please enter a number in the box below.", "Only whole numbers please");
 
-	userInput = parseInt(userInput);
-	userInput = userInput-(userInput%1);
+    userInput = parseInt(userInput);
+    userInput = userInput - (userInput % 1);
 
-	userInput = +userInput;
-
-
-	var response = "";
-
-	function display(input){
-		var p = document.createElement("p");
-		p.innerHTML=input;
-		$(".container").append(p);
-
-	};
-
-	var p = document.createElement("p");
-
-	
-	for (var i =1;i<userInput+1;i++){
+    userInput = +userInput;
 
 
-		if(i%3==0 && i%5==0){
+    var response = "";
 
-			display("fizz buzz");
-			
-		}
-		else if(i%3==0){
-				display("fizz");
-		}
+    function display(input) {
+        var p = document.createElement("p");
+        p.innerHTML = input;
+        $(".container").append(p);
 
-		else if (i%5==0){
-			
-			display("buzz");
-		}
-		
+    };
 
-		else{
-			
-			display(i);
-		}
+    var p = document.createElement("p");
+
+
+    for (var i = 1; i < userInput + 1; i++) {
+
+
+        if (i % 3 == 0 && i % 5 == 0) {
+
+            display("fizz buzz");
+
+        } else if (i % 3 == 0) {
+            display("fizz");
+        } else if (i % 5 == 0) {
+
+            display("buzz");
+        } else {
+
+            display(i);
+        }
 
 
 
-	};
+    };
 
 });
